d196b353aa7d060111ea5f1adc8cf3669f8a6d3b
IOhannes m zmölnig <zmoelnig@iem.at>
2023-01-10 15:38:02 +0100

    blackened code
    except for encryption_routine.py which becomes unreadable (but beautiful)

--- mos/download_files.py
+++ mos/download_files.py
@@ -10,12 +10,14 @@ import uuid
 
 from ast import literal_eval
 from bs4 import BeautifulSoup as bs
+
 try:
     from mos.encryption_routine import encryption_routine
     from mos.headers import csrf_request_headers, download_image_headers
 except ModuleNotFoundError:
     from encryption_routine import encryption_routine
     from headers import csrf_request_headers, download_image_headers
+
 
 class Downloader:
     def __init__(self, record_URL, file_range, base_images_dir):
@@ -27,7 +29,7 @@ class Downloader:
         self.archive_directory_name = None
         self.image_URLs_and_labels = None
         self.csrf_token = None
-        self.CRAWL_SPEED = 2 # 2 second delay between each archive request
+        self.CRAWL_SPEED = 2  # 2 second delay between each archive request
 
     @classmethod
     def log_error_and_exit(cls, error_message):
@@ -36,7 +38,9 @@ class Downloader:
 
     def create_archive_directory(self):
         # archive_directory_path = f"{self.base_images_dir}\\{self.archive_directory_name}"
-        archive_directory_path = os.path.join(self.base_images_dir,self.archive_directory_name)
+        archive_directory_path = os.path.join(
+            self.base_images_dir, self.archive_directory_name
+        )
 
         if not os.path.exists(self.base_images_dir):
             os.mkdir(self.base_images_dir)
@@ -53,9 +57,13 @@ class Downloader:
                 self.parse_archive_name(response.text)
                 self.create_archive_directory()
             else:
-                self.log_error_and_exit(f"{response.status_code} Status Code Fetching CSRF Token, Exiting")
+                self.log_error_and_exit(
+                    f"{response.status_code} Status Code Fetching CSRF Token, Exiting"
+                )
         except Exception:
-            self.log_error_and_exit(f"Unexpected Error Fetching Record Page, Exiting\n{traceback.format_exc()}")
+            self.log_error_and_exit(
+                f"Unexpected Error Fetching Record Page, Exiting\n{traceback.format_exc()}"
+            )
 
     def get_csrf_token(self):
         csrf_token = self.session.cookies.get_dict().get("shared_csrftoken")
@@ -74,32 +82,58 @@ class Downloader:
         full_files_list = literal_eval(start_files_list.split('"],')[0].strip() + '"]')
 
         start_labels_list = record_reponse_text.split('"labels":')[1]
-        full_labels_list = literal_eval(start_labels_list.split('"],')[0].strip() + '"]')
+        full_labels_list = literal_eval(
+            start_labels_list.split('"],')[0].strip() + '"]'
+        )
 
         self.image_URLs_and_labels = tuple(zip(full_files_list, full_labels_list))
 
         # if a range is provided and the range is less than the number of scraped files, use it
-        if self.file_range is not None and self.file_range < len(self.image_URLs_and_labels):
-            self.image_URLs_and_labels = self.image_URLs_and_labels[0:self.file_range]
+        if self.file_range is not None and self.file_range < len(
+            self.image_URLs_and_labels
+        ):
+            self.image_URLs_and_labels = self.image_URLs_and_labels[0 : self.file_range]
 
-        logging.info(f"Fetched List of {len(self.image_URLs_and_labels)} Images From '{self.record_URL}'")
+        logging.info(
+            f"Fetched List of {len(self.image_URLs_and_labels)} Images From '{self.record_URL}'"
+        )
 
     def parse_archive_name(self, record_reponse_text):
         try:
             soup = bs(record_reponse_text, "html.parser")
-            archive_category = soup.find("table", {"class": "table table-register-data"}).find("a").text.strip()
-            archive_category = "".join([char for char in archive_category if char.isalnum()]) # removing characters that could cause errors when writing creating dir
-            archive_id = record_reponse_text.split("Archival identifier<td>")[1].split("\n")[0].strip()
+            archive_category = (
+                soup.find("table", {"class": "table table-register-data"})
+                .find("a")
+                .text.strip()
+            )
+            archive_category = "".join(
+                [char for char in archive_category if char.isalnum()]
+            )  # removing characters that could cause errors when writing creating dir
+            archive_id = (
+                record_reponse_text.split("Archival identifier<td>")[1]
+                .split("\n")[0]
+                .strip()
+            )
             archive_id = "".join([char for char in archive_id if char.isalnum()])
             self.archive_directory_name = archive_category + "_" + archive_id
         except Exception as e:
-            print("Error Parsing Archive Category and ID, Creating Random Directory Name")
-            self.archive_directory_name = str(uuid.uuid4())[0:18] # first 18 chars of a random UUID
+            print(
+                "Error Parsing Archive Category and ID, Creating Random Directory Name"
+            )
+            self.archive_directory_name = str(uuid.uuid4())[
+                0:18
+            ]  # first 18 chars of a random UUID
 
     def save_image(self, image_content, image_label, file_number):
-        logging.info(f"Downloaded File {file_number} of {len(self.image_URLs_and_labels)}")
-        file_path = f"{self.base_images_dir}\\{self.archive_directory_name}\\{image_label}.jpg"
-        file_path = os.path.join(self.base_images_dir,self.archive_directory_name,f"{image_label}.jpg")
+        logging.info(
+            f"Downloaded File {file_number} of {len(self.image_URLs_and_labels)}"
+        )
+        file_path = (
+            f"{self.base_images_dir}\\{self.archive_directory_name}\\{image_label}.jpg"
+        )
+        file_path = os.path.join(
+            self.base_images_dir, self.archive_directory_name, f"{image_label}.jpg"
+        )
 
         with open(file_path, "wb") as f:
             f.write(image_content)
@@ -116,27 +150,39 @@ class Downloader:
                     if response.status_code == 200:
                         self.save_image(response.content, image_label, file_number)
                     else:
-                        logging.info(f"Skipping File {file_number} ({response.status_code} Response)")
+                        logging.info(
+                            f"Skipping File {file_number} ({response.status_code} Response)"
+                        )
                     time.sleep(self.CRAWL_SPEED)
                     break
                 except requests.exceptions.ConnectionError:
                     if request_attempts < 3:
                         request_attempts += 1
-                        logging.info(f"Retrying File {file_number}, Attempt ({request_attempts})")
+                        logging.info(
+                            f"Retrying File {file_number}, Attempt ({request_attempts})"
+                        )
                     else:
-                        logging.info(f"Skipping File {file_number}, {request_attempts} failed attempts")
+                        logging.info(
+                            f"Skipping File {file_number}, {request_attempts} failed attempts"
+                        )
                 except Exception:
-                    logging.info(f"Skipping File {file_number}\n ({traceback.format_exc()})")
+                    logging.info(
+                        f"Skipping File {file_number}\n ({traceback.format_exc()})"
+                    )
                     break
 
 
 if __name__ == "__main__":
     logging.basicConfig(
-        format='%(asctime)s %(levelname)-8s %(message)s',
+        format="%(asctime)s %(levelname)-8s %(message)s",
         level=logging.INFO,
-        datefmt='%Y-%m-%d %H:%M:%S'
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
 
-    download = Downloader("https://data.matricula-online.eu/en/deutschland/akmb/militaerkirchenbuecher/0002/?pg=1", None, "./images")
+    download = Downloader(
+        "https://data.matricula-online.eu/en/deutschland/akmb/militaerkirchenbuecher/0002/?pg=1",
+        None,
+        "./images",
+    )
     download.fetch_record_page()
     download.download_files()
--- mos/headers.py
+++ mos/headers.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
 
+
 def csrf_request_headers():
     return {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Connection': 'keep-alive',
-        'Upgrade-Insecure-Requests': '1',
-        'Pragma': 'no-cache',
-        'Cache-Control': 'no-cache',
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Pragma": "no-cache",
+        "Cache-Control": "no-cache",
     }
+
 
 def download_image_headers():
     return {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0',
-        'Accept': '*/*',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Origin': 'https://data.matricula-online.eu',
-        'Connection': 'keep-alive',
-        'Referer': 'https://data.matricula-online.eu/',
-        'Pragma': 'no-cache',
-        'Cache-Control': 'no-cache',
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0",
+        "Accept": "*/*",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Origin": "https://data.matricula-online.eu",
+        "Connection": "keep-alive",
+        "Referer": "https://data.matricula-online.eu/",
+        "Pragma": "no-cache",
+        "Cache-Control": "no-cache",
     }
